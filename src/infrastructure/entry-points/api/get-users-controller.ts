14256b7568f9ee89f4d1ad807df2ed05f06d95e1
Jorge Ortiz <jaortiz@nexos-software.com.co>
2021-11-26 17:52:07 -0500

    consultar usuario por token

--- src/infrastructure/entry-points/api/get-users-controller.ts
+++ src/infrastructure/entry-points/api/get-users-controller.ts
@@ -1,4 +1,4 @@
-import {Mapping, Get, Inject} from "@tsclean/core";
+import {Mapping, Get, Inject, Param} from "@tsclean/core";
 import {GetUsersServiceImpl} from "@/domain/use-cases/impl/get-users-service-impl";
 import { GET_USERS_SERVICE } from "@/domain/use-cases/get-users-service";
 
@@ -14,5 +14,10 @@ export class GetUsersController {
     @Get()
     async getUsersController(): Promise<any> {
         return await this.getUsersService.getUsers();
+    }
+
+    @Get("/tokenaccess/:token")
+    async getUsersByTokenAccessController(@Param("token") token: string): Promise<any> {
+        return await this.getUsersService.getUserByAccessToken(token);
     }
 }
